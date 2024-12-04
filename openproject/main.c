70075ca235a740ce87d3a41e05bca45e393275c4
Kim Taekgyun <tudr47@gmail.com>
2024-12-04 21:10:09 +0900

    입력처리

--- openproject/main.c
+++ openproject/main.c
@@ -6,6 +6,11 @@ srand(time(0)); // 랜덤 시드 설정
 int targetNumber = rand() % 100 + 1; // 1부터 100까지의 랜덤 숫자
 printf("랜덤 숫자를 생성했습니다: %d\n", targetNumber); // 테스트 출력
 
+int guess;
+printf("숫자를 입력하세요: ");
+scanf("%d", &guess);
+printf("입력한 숫자: %d\n", guess); // 테스트 출력
+
 int main() {
     printf("숫자 추측 게임에 오신 것을 환영합니다!\n");
     return 0;
